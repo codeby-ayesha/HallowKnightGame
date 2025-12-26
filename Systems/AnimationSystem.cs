cb6666e3d5c3a88d33cb02a2a2dcca911c791867
Ayesha Arhsad <ayeshaarshadali06@gmail.com>
2025-12-25 23:53:48 -0800

    Update AnimationSystem.cs

--- Systems/AnimationSystem.cs
+++ Systems/AnimationSystem.cs
@@ -13,20 +13,20 @@ namespace GameFrameWork
 
         public void Update(IAnimation obj)
         {
-            // 1️⃣ Current state check karo
+            //  Current state check karo
             string state = obj.CurrentState;
 
-            // 2️⃣ Dictionary se us state ki list uthao
+            //  Dictionary se us state ki list uthao
             List<Image> frames = obj.Animations[state];
 
-            // 3️⃣ FrameIndex ko update karo (ek frame aage)
+            //  FrameIndex ko update karo (ek frame aage)
             obj.CurrentFrame++;
             if (obj.CurrentFrame >= frames.Count)
             {
                 obj.CurrentFrame = 0; // loop back
             }
 
-            // 4️⃣ Current frame ko sprite pe set karo
+            //  Current frame ko sprite pe set karo
             obj.Sprite = frames[obj.CurrentFrame];
         }
     }
